cd2e39b8bd889e2465f6a3a3ccf0a96d88fa9c03
ALanch22 <alanhau48@gmail.com>
2025-06-19 18:06:39 -0600

    Actualización de archivos modificados

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:admin_usuario/main.dart';
+import 'package:tarea_caballero/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+
+    
+    await tester.pumpWidget(const FidelizacionApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
